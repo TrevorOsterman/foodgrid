597c5bb3526857320de7d221c0b6a87b9546287d
Trevor Osterman <trevormosterman@gmail.com>
2019-07-10 19:41:04 -0700

    styling finalized, js cleaned up

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
 const yelpKey = "Oi573GrBv4w13yNAYNhpBCjWDx3IZIYGsxLn7vvj0oIXNkN-NagE5aB8oRgnC0-xSyac9xx9J39HCKSRf8bK6t6gEc7Y7zVbE1hGA47T-5dFyrMeJM0V2EZEm-cbXXYx";
 const yelpURL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search';
 
+/* Formats parameters for API call */
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
     .map(key => `${key}=${params[key]}`)
   return queryItems.join('&');
 }
 
+/* API call */
 function dataReturn(loc){
-  console.log('dataReturn working');
-  const yelpParams = {
-    location: loc,
-  };
-
+  const yelpParams = {location: loc,};
   const yelpString = formatQueryParams(yelpParams);
-  // const eventString = formatQueryParams(eventParams)
-
   const urlYelp = yelpURL + '?sortby=rating&limit=50&term=food&' + yelpString;
-  // const urlEvent = eventURL + '?' + eventString;
-
-  console.log(urlYelp);
-
   const options = {
-    headers: {
-      'authorization': `Bearer ${yelpKey}`,
-    }
+    headers: {'authorization': `Bearer ${yelpKey}`,}
   };
 
-  /* Yelp API call */
   fetch(urlYelp, options)
     .then(response => {
       if (response.ok) {
@@ -41,32 +30,31 @@ function dataReturn(loc){
     });
 }
 
+/* Hide landing page, display results using jQuery */
 function displayResults(responseJson) {
-  console.log(responseJson);
   $('#query').css({'display': 'none'});
   $(".grid").empty();
   for (let i = 0; i < responseJson.businesses.length; i++){
-    console.log(responseJson.businesses[i].name);
     $('.grid').append(`<a class='thumb' href="${responseJson.businesses[i].url}" target='_blank'><img src="${responseJson.businesses[i].image_url}" class='thumb' alt='biz-thumb'></a>`);
     let cw = $('.thumb').width();
     $('.thumb').css({'height':cw+'px'});
     $('.search2').removeClass('hidden');
     $('#search-link').removeClass('hidden');
+    $('#goback').removeClass('hidden');
   }
 }
 
+// Search bar functionality
 function searchListen() {
-  console.log('searchListen working')
   $('body').on('click', '#search-link', function(event){
     event.preventDefault();
     const loc = $('#searchbar').val();
-    // const spec = $('#spec').val();
     dataReturn(loc);
   })
 }
 
+// Initial form listener
 function formListen() {
-  console.log('formListen working')
   $('body').on('click', '.submit-button', function(event){
     event.preventDefault();
     const loc = $('#location').val();
